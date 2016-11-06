08917ccd2bedd16769df3558ac58bbbe8390e658
Felix Kopp <felix-source@phorax.com>
2016-11-06 16:03:48 +0100

    [TASK] Version constraints - kick-start

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '5.5.0-0.0.0',
-            'typo3' => '7.6.0-7.6.99',
+            'typo3' => '7.6.0-8.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
