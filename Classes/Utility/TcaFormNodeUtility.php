78136cf0f936eec071bd657b9d3f613a4929e717
Thomas Luzat <thomas@luzat.com>
2021-04-04 17:00:31 +0200

    fix: Backend formhandler log entry display

--- Classes/Utility/TcaFormNodeUtility.php
+++ Classes/Utility/TcaFormNodeUtility.php
@@ -1,25 +1,28 @@
 <?php
+
 namespace Typoheads\Formhandler\Utility;
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Backend\Form\Element\AbstractFormElement;
 
 
 /**
  * UserFunc for rendering of log entry
  */
-class TcaFormNodeUtility extends \TYPO3\CMS\Backend\Form\Element\AbstractFormElement
+class TcaFormNodeUtility extends AbstractFormElement
 {
-    public function render()
-    {
-	    $result = $this->initializeResultArray();
-        $params = unserialize($this->data['databaseRow']['params']);
+  public function render()
+  {
+    $result = $this->initializeResultArray();
+    $params = unserialize($this->data['databaseRow']['params']);
 
-	    if (is_array($params))  {
-		    foreach($params as $key => $value)  {
-		        $result['html'] .= '<tr><td style="padding: 4px 10px; font-style: italic;">'. htmlspecialchars($key) .'</td><td style="padding: 4px 10px;">'. htmlspecialchars($value) .'</td>';
-		    }
-	    }
-	    $result['html'] = '<table>' . $result['html'] . '</table>';
-        return $result;
+    if (is_array($params)) {
+      foreach ($params as $key => $value) {
+        $result['html'] .= '<tr><td style="padding: 4px 10px; font-style: italic;">' . htmlspecialchars(
+            $key
+          ) . '</td><td style="padding: 4px 10px;">' . htmlspecialchars(is_array($value) ? implode(',', $value) : $value) . '</td>';
+      }
     }
+    $result['html'] = '<table>' . $result['html'] . '</table>';
+    return $result;
+  }
 }
